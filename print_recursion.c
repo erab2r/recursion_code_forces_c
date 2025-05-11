d16d853c4f750ec2a8e3736048e0f28c29e06e32
erab2r <shkera213@gmail.com>
2025-05-12 00:51:39 +0600

    first commit

---
+++ print_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void printLoveRecursion(int n) {
+    if (n == 0)
+        return;
+    
+    printf("I love Recursion\n");
+    printLoveRecursion(n - 1);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    printLoveRecursion(n);
+
+    return 0;
+}
